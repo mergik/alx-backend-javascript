5b65250db148a68ea42dff9200bc4a3fda65e2b4
mergik <gmerlix@yahoo.com>
2024-01-09 23:41:42 +0300

    mandatory tasks 1-12

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const value of array) {
+    array[array.indexOf(value)] = appendString + value;
+  }
+
+  return array;
+}
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+export default function returnHowManyArguments(...args) {
+  return arguments.length;
+}
