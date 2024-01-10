7e6922f40a6378cba1f37660b6e1ad837eda14f0
mergik <gmerlix@yahoo.com>
2024-01-10 12:31:15 +0300

    Rewrite script

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,39 +1,16 @@
 export default class HolbertonClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
+    this._size = size;
+    this._location = location;
   }
 
-  [Symbol.toPrimitive](obj) {
-    if (obj === 'number') {
-      return this.size;
+  [Symbol.toPrimitive](type) {
+    if (type === 'number') {
+      return (this._size);
     }
-    if (obj === 'string') {
-      return this.location;
+    if (type === 'string') {
+      return (this._location);
     }
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    if (typeof value === 'number') {
-      this._size = value;
-    } else {
-      throw new TypeError('Size must be a number');
-    }
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    if (typeof value === 'string') {
-      this._location = value;
-    } else {
-      throw new TypeError('Location must be a string');
-    }
+    return null;
   }
 }
